320013c8acf50766cb023f772ca09eb318d8e666
Dylan Curtis <Dcurtisj@gmail.com>
2023-01-08 22:03:52 -0800

    commit tree 3ab03692d4a76534fc3bc7cf0066992a2bc7fe11

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def oxford_comma(array)
+if array.length >2
+    last = array.pop 
+    array1= array.map{|str| str  + ", "}
+    #  binding.pry
+    
+array1 << "and " << last
+array1.join
+
+elsif array.length == 2 
+    last = array.pop 
+    array << " and " << last
+    array.join
+else
+    array.pop
+end
 
 end
